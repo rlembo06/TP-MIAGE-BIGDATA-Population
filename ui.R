d3fdf78870887f37426176475b26417c4f56d2f6
Romain Lembo <romainlembo06@gmail.com>
2018-12-29 15:59:51 +0100

    test create secondly plot

---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 60,
+                  value = 50)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot"),
+      plotOutput("testPlot")
+    )
+  )
+)
