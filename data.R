733fbcb1b8b9cf47fb614dd0c7b991347271ce76
Romain Lembo <romainlembo06@gmail.com>
2019-01-02 21:31:50 +0100

    add 11 - 17 stats

--- data.R
+++ data.R
@@ -7,9 +7,6 @@ mydatapop <- read.csv("./data/data.csv", sep=",")
 getIndexColumn <- function(df, col) { which(names(df) == col) }
 rangeBetweenColumByName <- function(df, col1, col2) { df[, getIndexColumn(df, col1) : getIndexColumn(df, col2)] }
 
-#GLOBAL : 
-DFpercentage65UpByYears_init <- data.frame("Years" = 1960:2010, "Europe" = 1960:2010, "World"= 1960:2010)
-
 # Population totale par pays
 totalPopulationByCountry <- mydatapop[mydatapop$Series.Code == 'SP.POP.TOTL',]
 totalPopulationByCountry_65Up <- mydatapop[mydatapop$Series.Code == 'SP.POP.65UP.TO',]
@@ -31,6 +28,11 @@ world <- totalPopulationByCountry[totalPopulationByCountry$Country.Code == 'WLD'
 # Moyenne population Europe de 1960 à 2010
 averagePopEurope60_10 <- rowMeans(rangeBetweenColumByName(europe, "X1960..YR1960.", "X2010..YR2010."))
 
+# -------------------------- #
+## Population de 1960 à 2010
+
+DFpercentage65UpByYears_init <- data.frame("Years" = 1960:2010, "Europe" = 1960:2010, "World"= 1960:2010)
+
 # Pourcentage +65 ans Monde de 1960 à 2010
 worldYears65Up <- rangeBetweenColumByName(world65Up, "X1960..YR1960.", "X2010..YR2010.")
 worldYears <- rangeBetweenColumByName(world, "X1960..YR1960.", "X2010..YR2010.")
@@ -47,6 +49,7 @@ temp <- as.data.frame(t(percentage65UpByYears_Europe))
 colnames(temp)[1] = "Europe"
 DFpercentage65UpByYears_init$Europe <- temp$Europe
 
+# Création de la data frame pour le plot
 DFpercentage65UpByYears_world <- DFpercentage65UpByYears_init %>% 
   mutate(region = 'World') %>% 
   mutate(percentage65Up = World) %>% 
@@ -57,13 +60,42 @@ DFpercentage65UpByYears_europe <- DFpercentage65UpByYears_init %>%
   mutate(percentage65Up = Europe) %>% 
   select(Years,region, percentage65Up)
 
-# Fusion DFpercentage65UpByYears_europe + DFpercentage65UpByYears_world :
 DFpercentage65UpByYears <- bind_rows(DFpercentage65UpByYears_world,DFpercentage65UpByYears_europe)
-  
-####
+
+# -------------------------- #
+
+# -------------------------- #
+## Population de 2011 à 2017
+
+DFpercentage65UpByYears_init <- data.frame("Years" = 2011:2017, "Europe" = 2011:2017, "World"= 2011:2017)
+
+# Pourcentage +65 ans Monde de 2011 à 2017
+worldYears65Up11_17 <- rangeBetweenColumByName(world65Up, "X2011..YR2011.", "X2017..YR2017.")
+worldYears11_17 <- rangeBetweenColumByName(world, "X2011..YR2011.", "X2017..YR2017.")
+percentage65UpByYears_World11_17 <- 100 * worldYears65Up11_17 / worldYears11_17
+temp <- as.data.frame(t(percentage65UpByYears_World11_17))
+colnames(temp)[1] = "World"
+DFpercentage65UpByYears_init$World <- temp$World
 
 # Pourcentage +65 ans Europe de 2011 à 2017
 europeYears65Up11_17 <- rangeBetweenColumByName(europe65Up, "X2011..YR2011.", "X2017..YR2017.")
 europeYears11_17 <- rangeBetweenColumByName(europe, "X2011..YR2011.", "X2017..YR2017.")
 percentage65UpByYears_Europe11_17 <- 100 * europeYears65Up11_17 / europeYears11_17
-#plotPercentage65UpByYears_Europe11_17 <- as.data.frame(t(percentage65UpByYears_Europe11_17)) # Inversion tableau
+temp <- as.data.frame(t(percentage65UpByYears_Europe11_17))
+colnames(temp)[1] = "Europe"
+DFpercentage65UpByYears_init$Europe <- temp$Europe
+
+# Création de la data frame pour le plot
+DFpercentage65UpByYears_world <- DFpercentage65UpByYears_init %>% 
+  mutate(region = 'World') %>% 
+  mutate(percentage65Up = World) %>% 
+  select(Years,region, percentage65Up)
+
+DFpercentage65UpByYears_europe <- DFpercentage65UpByYears_init %>% 
+  mutate(region = 'Europe') %>% 
+  mutate(percentage65Up = Europe) %>% 
+  select(Years,region, percentage65Up)
+
+DFpercentage65UpByYears11_17 <- bind_rows(DFpercentage65UpByYears_world,DFpercentage65UpByYears_europe)
+
+# -------------------------- #
--- server.R
+++ server.R
@@ -7,7 +7,7 @@ function(input, output) {
   
   output$plotPercent65Up60_10<- renderPlot({
     data <- DFpercentage65UpByYears
-    title <- ggtitle("Evolution de la proportion des +65 ans")
+    title <- ggtitle("Evolution de la proportion des +65 ans (1960 - 2010)")
     type <- geom_line(size=2)
     args <- aes(
       x = Years, 
@@ -15,9 +15,30 @@ function(input, output) {
       group = region, 
       color = region
     )
-    legend <- theme(legend.position="top")
+    legend_title <- theme(
+      legend.position="top",
+      plot.title = element_text(size = 20, face = "bold")
+    )
     
-    ggplot(data, args) + type + title + legend + xlab("Années") + ylab("Pourcentage + 65 ans")
+    ggplot(data, args) + type + title + legend_title + xlab("Années") + ylab("Pourcentage + 65 ans")
+  })
+  
+  output$plotPercent65Up11_17<- renderPlot({
+    data <- DFpercentage65UpByYears11_17
+    title <- ggtitle("Evolution de la proportion des +65 ans (2011 - 2017)")
+    type <- geom_line(size=2)
+    args <- aes(
+      x = Years, 
+      y = percentage65Up, 
+      group = region, 
+      color = region
+    )
+    legend_title <- theme(
+      legend.position="top",
+      plot.title = element_text(size = 20, face = "bold")
+    )
+    
+    ggplot(data, args) + type + title + legend_title + xlab("Années") + ylab("Pourcentage + 65 ans")
   })
   
 }
